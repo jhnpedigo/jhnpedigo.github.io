0f10ee94dc84c912d52d9c96492f128cc48f89a8
jhnpedigo <46077047+jhnpedigo@users.noreply.github.com>
2026-03-09 02:27:28 +0000

    Deploying to gh-pages from @ jhnpedigo/jhnpedigo.github.io@33e9ac498b5913d6072ceb8a34ea2ca73179709c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jhnpedigo/folio",
+    "data-repo": "jhnpedigo/jhnpedigo.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
