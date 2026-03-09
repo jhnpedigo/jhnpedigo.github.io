fb74b41de0f15312d8e8d504b851b5fc93df67a4
jhnpedigo <46077047+jhnpedigo@users.noreply.github.com>
2026-03-09 03:16:34 +0000

    Deploying to gh-pages from @ jhnpedigo/jhnpedigo.github.io@00107feec7ad1d1c4ea8c1ac7f996be8038d7bd6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
